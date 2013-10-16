a575e7df771c82cf1018fda8b2e2e30317563ecf
srgg6701 <srgg6701@gmail.com>
2013-10-17 00:04:22 +0400

    Причёсаны файлы в дир. scripts.

--- scripts/actions.php
+++ scripts/actions.php
@@ -2,45 +2,26 @@
     require_once '../config.php';
     require_once 'connect_db.php';
     
-    $test=true;
-	if($test){
-        var_dump($_POST);
-        var_dump($_FILES['pic']);
-    }
+    $Db=new Db();
+	$connect=$Db->getConnect();
+	    
     /**
      * Проверить уникальность записи (логин, емэйл)
      */
     function checkSingleField($field){
         global $connect,$test;
-        $cnt = $connect->query("SELECT COUNT(*) 
+        return $connect->query("SELECT COUNT(*) 
                 FROM users WHERE $field = '$_POST[$field]'")
-                       ->fetchColumn();
-        if($cnt&&$test)
-            echo "<div>Запись НЕ уникальна. Всего записей: $cnt</div>";        
-        return $cnt;
+                       ->fetchColumn();       
     }
     /**
-     * Если не можем получить last_insert_id прямо:
+     * Если не можем получить last_insert_id прямо (к сожалению, такое не исключено):
      */
     function getLastId($table='users'){
         global $connect,$test;
         return $connect->query("SELECT MAX(id) FROM $table")->fetchColumn();
     }
-    /**
-     * Вывести результат валидации в тестовом режиме:
-     */
-    function showTestValidationResult($key,$val,$valid=false){
-        global $test;
-        if($test) {
-            echo "<div>Данные ";
-            if(!$valid) echo " <b style='color:red'>НЕ</b> ";
-            echo " валидны:<br><b>$key</b> => $val</div>";
-        }
-    }
     
-    $Db=new Db();
-	$connect=$Db->getConnect();
-	
     // создать массивы неправильных значений
     // (невалидные, занятые, проблемные)
     $Invalids=explode(",",Invalids); //'invalids,taken,xtra'
@@ -49,9 +30,7 @@
         unset($_SESSION[$invld]); // удалить старые данные, если остались
     }
     unset($_SESSION['valid_data']);
-    //echo "<h1>SESSION:</h1>";
-    //var_dump($_SESSION);
-    //echo "<hr>";
+    
     // контейнер данных для добавления в таблицу:
     $dataToInsert=array();
     foreach ($_POST as $key => $val){        
@@ -59,33 +38,26 @@
             case 'login':
                 // проверить логин на уникальность:
                 if (checkSingleField('login')){
-                    $taken[$key]=$val;                    
+                    $taken[$key]=$val; // пополнить массив занятых значений                    
                 }else{
                     if(preg_match($filters[$key], $val)){
-                        $invalids[$key]=$val;
-                        showTestValidationResult($key,$val);
-                    }else{
+                        $invalids[$key]=$val; // пополнить массив невалидных значений
+                    }else{ // пополнить массив валидных значений для добавления в таблицу users
                         $dataToInsert[$key]=$val;
-                        showTestValidationResult($key,$val,true);
                     }
                 }
                 break;
             case 'password'://password2
-                //
                 if(preg_match($filters[$key], $val)){
                     $invalids[$key]=$val;
-                    showTestValidationResult($key,$val);
                 }else{
                     $dataToInsert[$key] = md5($val);
-                    showTestValidationResult($key,md5($val),true);
                 }
                 if(preg_match($filters[$key], $_POST['password2'])){
                     $invalids[$key.'2']=$_POST['password2'];
-                    showTestValidationResult($key.'2',$_POST['password2']);
                 }
-                if($val!=$_POST['password2'])
-                    $xtra['pass_diff']=PassDiff;
-                
+                if($val!=$_POST['password2']) // пополнить массив дополнительнрых невалидных значений
+                    $xtra['pass_diff']=PassDiff;                 
                 break;
             case 'email':
                 // проверить на занятость и валидность:
@@ -93,39 +65,31 @@
                     $taken[$key]=$val;
                 }else{
                     if(!filter_var($val, FILTER_VALIDATE_EMAIL)){
-                        $invalids[$key]=$val;   
-                        showTestValidationResult($key,$val);
+                        $invalids[$key]=$val;
                     }else{
                         $dataToInsert[$key]=$val;
-                        showTestValidationResult($key,$val, true);
                     }
                 }
                 break;
             case 'name': 
                 if(preg_match($filters[$key], $val)){
                     $invalids[$key]=$val;
-                    showTestValidationResult($key,$val);
                 }else{
                     $dataToInsert[$key]=$val;
-                    showTestValidationResult($key,$val, true);
                 }
             break;
             case 'phone':
-                //
                 if(preg_match($filters[$key], $val)){
                     $invalids[$key]=$val;
-                    showTestValidationResult($key,$val);
                 }else{
-                    //$val = str_replace(" ", "", $val);
                     $dataToInsert[$key]=$val;
-                    showTestValidationResult($key,$val, true);
                 }
             break;
         } 
     }    
     /**
      *  Выяснить, является ли валидным расширение загруженного файла. 
-     *  Если нет, будем возвращаться назад - к форме заполнения данных
+     *  Если нет, также будем возвращаться назад - к форме заполнения данных
      */
     if($file_data=$_FILES['pic']){
         // проверим расширение загруженного файла:
@@ -133,30 +97,15 @@
         $ext = array_pop($arr_fname);
         if(!in_array($ext, explode(",",PicExt))){
             $xtra['wrong_pic']=WrongPicExt; // дополним один из массивов невалидных полей
-            echo "<h2>ext = $ext</h2>";
         }
     }
     
-    $wrng=0;
+    $wrng=0; // счётчик ошибок
     // сохранить невалидные данные в сессии, чтобы показать юзеру при возврате
-    // или удалить старые данные из сессии, если всё ОК.
     foreach($Invalids as $inv) { // 'invalids','taken','xtra'
-        if(!empty(${$inv})){ // $invalids, $taken, $xtra
-            if($test) {
-                echo "<h4>line ".__LINE__.": Невалидные данные ($inv):</h4>";
-                var_dump(${$inv});
-            }
-            
+        if(!empty(${$inv})){ // $invalids, $taken, $xtra            
             $_SESSION[$inv]=${$inv};
-            $wrng++;
-            
-            if(!( $inv=='invalids'
-                   && (isset($invalids['password'])||isset($invalids['password2']))
-              )) $_SESSION[$inv]=${$inv};
-            
-            echo '<h3>invalid type: '.$inv.'</h3>';
-            var_dump($_SESSION[$inv]);
-            var_dump(${$inv});
+            $wrng++;            
         }
     }
     // если никаких ошибок не выявлено, будем добавлять записи в таблицу:        
@@ -166,6 +115,7 @@
             $keys[]=$field;
             $vals[]=$value;
         }
+        // сформируем SQL-запрос:
         $fields='`'.implode('`, `',$keys).'`';
         $values="'".implode("', '",$vals)."'";
         $query="INSERT INTO users (".$fields.", `datetime`) 
@@ -182,15 +132,14 @@
         if(!$last_id=$Db->getConnect()->lastInsertId()){
             $_SESSION['user_id']=$last_id=getLastId();            
         }
-                
-        if($file_data&&!isset($xtra['wrong_pic'])){ // если всё ОК и есть файлы, будем размещать их:
+        // если всё ОК и есть файлы, будем размещать их:        
+        if($file_data&&!isset($xtra['wrong_pic'])){ 
             /*  заменим все подозрительные символы в имени файла.
                 Это нужно нам только для информативных целей. Сам файл будет 
                 сохранён под именем последней добавленной в таблицу записи */
-            $file_name = preg_replace("/[^а-яёА-ЯЁa-zA-Z0-9\-\._]/", "", $file_data['name']);
+            $file_name = preg_replace("/[^а-пр-яА-ПР-Яa-zA-Z0-9\-\._]/", "", $file_data['name']);
             $query="INSERT INTO pix (filename,user_id) VALUES ('$file_name',".$last_id.")";
             try{
-                if($test) echo "<pre>$query</pre>";
                 $Db->execute($query);
             }catch(PDOExecption $e){
                 echo 'Ошибка добавления записи: '.$e->getMessage();
@@ -203,37 +152,33 @@
             // сохранить файл под индексом записи для него в таблице pix
             if (!move_uploaded_file($file_data['tmp_name'], $file_location )) 
                 echo "<div style='color:orange'>...Ошибка загрузки файла $file_location</div>";
-
         }
         /*  удалим ранее сохранённые валидные данные (если есть).
             Они создаются в случае, если есть также и НЕвалидные данные.
             Это нужно для того, чтобы заполнить поля формы при возврате после
-            того, как невалидные данные были обнаружены. Потому что система
-            должна назначать соответствующий стилевой атрибут невалидным данным,
-            т.о., при заполнении ячеек мы должны знать, какие из сохранённых в
-            сессии валидны, а какие - нет. */
+            того, как невалидные данные были обнаружены. Потому что:
+            *   юзер должен знать, что он указал неправильно (это очевидно, да); 
+            *   система должна назначать соответствующий стилевой атрибут 
+                невалидным данным. 
+            т.о., мы подставляем в ячейки как валидные, так и НЕ валидные данные,
+            но последние должны быть выделены цветом. 
+            Но (снова) это только в случае, если они (последние) есть. 
+            Т.е., это - условие следующего блока.
+         */
         unset($_SESSION['valid_data']);
-        //var_dump($_SESSION);
+        // директимся после успешной регистрации:
         header("location: ".SITE_ROOT."account?register=ok");        
-    }else{        
+    }else{  // всё не так хорошо, как хотелось бы :)        
         /*  удалить из сессии и массивов валидных данных пароли, поскольку:
             - пароль уже обработан функцией md5()
-            - пароли при возврате (к форме) не сохраняют */
+            - пароли при возврате (к форме) не должны отображаться всё равно */
         unset($dataToInsert['password']);
         unset($dataToInsert['password2']);
         unset($_SESSION['invalid']['password']);
         unset($_SESSION['invalid']['password2']);
-        
+        // сохраним валидные значения для подстановки в поля формы:
+        // (невалидные уже сохранены)
         $_SESSION['valid_data']=$dataToInsert;
-        if($test){
-            echo "<h4>line ".__LINE__.": Невалидные данные:</h4>";
-            foreach($Invalids as $inv) // 'invalids','taken','xtra'
-                if(isset($_SESSION[$inv])){
-                    echo "<div><b>{$inv}:</b></div>
-                    <h3 style='color:green'>Сохранено в сессии:</h3>";
-                    var_dump($_SESSION[$inv]);
-                }
-            echo "<div>redirect: <a href=\"".SITE_ROOT."return\">".SITE_ROOT."return</a></div><hr>";
-        }else
-            header("location: ".SITE_ROOT."return");
+        // директимся после провала миссии:
+        header("location: ".SITE_ROOT."return");
     }
